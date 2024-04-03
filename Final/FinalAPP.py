441d2675762f9bb883638d1b0b677c3de3ea6b55
Themadhood's Codes <148127209+Themadhood@users.noreply.github.com>
2024-04-03 12:03:58 -0500

    Update FinalAPP.py (#1)

--- Final/FinalAPP.py
+++ Final/FinalAPP.py
@@ -16,6 +16,51 @@ class Widget(QWidget):
         self.ui.setupUi(self)
 
 
+
+class CReadFile:
+    def GetLists(self):
+        #read input.txt file alow for speshal chars
+        #split read in to two stacks with first line =ing 1
+        #return the 2 stacks
+        pass
+        
+            
+class CRandom:
+    def __init__(self,oddlst:list,evenlst:list):
+        self.stack_odd = oddlst
+        self.stack_even = evenlst
+
+        self.stack_temp = []
+
+    def SetLsts(self,oddlst:list=None,evenlst:list=None):
+        """
+sets a or both lst to a new lst
+"""
+        if oddlst != None:
+            self.stack_odd = oddlst
+        elif evenlst != None:
+            self.stack_even = evenlst
+
+    def Selection(self,number:int):
+        #randomly select a movie from bolth stacks
+        #add bolth to self.stack_temp
+        #determin if number is odd or even
+        #take corisponding movie from self.stack_temp
+        #return remaing movie to its lst
+        #delete the selected movie from its lst
+        #return selected movie
+        pass
+
+    def UpdateLbl(self):
+        #update lable with a movie title
+        pass
+
+
+class CFantasyMovies:
+    def __init__(self):
+        #get textbox input
+        pass
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     widget = Widget()

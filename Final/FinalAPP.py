c62085023868ce18f0f84332e23c75acb6799133
Themadhood's Codes <148127209+Themadhood@users.noreply.github.com>
2024-04-20 20:35:43 -0500

    Update FinalAPP.py (#23)

--- Final/FinalAPP.py
+++ Final/FinalAPP.py
@@ -9,38 +9,13 @@ from PySide6.QtWidgets import QApplication, QWidget
 #     pyside2-uic form.ui -o ui_form.py
 from ui_form import Ui_Widget
 
-class Widget(QWidget):
-    def __init__(self, parent=None):
-        super(Widget, self).__init__(parent)
 
-        # Set window flags to remove the title bar and frame
-        self.setWindowFlags(Qt.FramelessWindowHint)
+class CFantasyMovies:
+    def __init__(self,lbl):
+        self._lbl = lbl
+    def printMovie(self,txt):
+        self._lbl.setText(txt)
 
-
-        self.ui = Ui_Widget()
-        self.ui.setupUi(self)
-
-        # Connect the clicked signal to the slot
-        self.ui.exit_button.clicked.connect(self.close_application)
-
-        # Connect the 'clicked' signal of the ok_button to the slot
-        self.ui.ok_button.clicked.connect(self.on_ok_button_clicked)
-
-    def close_application(self):
-         # This slot will be called when the button is clicked
-        self.close()  # This will close the application window
-
-    def on_ok_button_clicked(self):
-        # This method will be called every time the OK button is clicked
-        # Insert the functionality you want here
-        user_input = self.ui.userNum.text()  # Assuming userNum is your QLineEdit
-        # You can now use the input to do something, like showing a movie name
-        # For example, if you have a method that gets a movie name based on a number
-        movie_name = self.get_movie_name_from_number(user_input)
-        self.ui.movieNameLabel.setText(movie_name)  # Assuming movieNameLabel is your QLabel
-
-    def get_movie_name_from_number(self, number):
-       pass
 
 
 
@@ -68,12 +43,57 @@ class CReadFile:
         self._Split(movies)
         
         return self.stack_odd,self.stack_even
+
+
+
+
+class Widget(QWidget):
+    def __init__(self, parent=None):
+        super(Widget, self).__init__(parent)
+
+        # Set window flags to remove the title bar and frame
+        self.setWindowFlags(Qt.FramelessWindowHint)
+
+
+        self.ui = Ui_Widget()
+        self.ui.setupUi(self)
+
+        # Connect the clicked signal to the slot
+        self.ui.exit_button.clicked.connect(self.close_application)
+
+        # Connect the 'clicked' signal of the ok_button to the slot
+        self.ui.ok_button.clicked.connect(self.on_ok_button_clicked)
+
+        self.Print = CFantasyMovies(lbl = self.ui.movieNameLabel)
+        self.ClassReed = CReadFile()
+        self.RandSelect = CRandom()
+
+    def close_application(self):
+         # This slot will be called when the button is clicked
+        self.close()  # This will close the application window
+
+    def on_ok_button_clicked(self):
+        # This method will be called every time the OK button is clicked
+        # Insert the functionality you want here
+        user_input = self.ui.userNum.text()  # Assuming userNum is your QLineEdit
+        # You can now use the input to do something, like showing a movie name
+        # For example, if you have a method that gets a movie name based on a number
+        movie_name = self.get_movie_name_from_number(user_input)
+        self.Print.printMovie(txt = movie_name)  # Assuming movieNameLabel is your QLabel
+
+    def get_movie_name_from_number(self, number):
+       return self.RandSelect.Selection(number = number)
+
+    def Read(self):
+        odd,even = self.ClassReed.GetLists()
+        self.RandSelect.SetLsts(oddlst=odd, evenlst=even)
+
         
             
 class CRandom:
-    def __init__(self,oddlst:list,evenlst:list):
-        self.stack_odd = oddlst
-        self.stack_even = evenlst
+    def __init__(self):
+        self.stack_odd = []
+        self.stack_even = []
 
         self.stack_temp = []
 
@@ -96,12 +116,6 @@ sets a or both lst to a new lst
         #return selected movie
         pass
 
-
-class CFantasyMovies:
-    def __init__(self,lbl):
-        self._lbl = lbl
-    def printMovie(self,txt):
-        self._lbl.setText(txt)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
